3c3c132e13c2362088ab6a50438daeea822fb6d2
Samuel Klas <klassam95@gmail.com>
2024-10-12 21:09:16 +0300

    fixed timer

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [userInput, setUserInput] = useState<string>("");
   const targetText: string = "lorem ipsum";
+  const [timer, setTimer] = useState<number>(30);
+  const [isStarted, setIsStarted] = useState<boolean>(false);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserInput(e.target.value);
@@ -13,6 +15,10 @@ export default function Home() {
   const renderText = () => {
     return targetText.split("").map((char, index) => {
       const userChar = userInput[index];
+
+      if (userChar === char) {
+      } else {
+      }
 
       // Tarkistetaan onko kirjain oikein vai väärin
       const color = userChar === char ? "green" : userChar ? "red" : "black";
@@ -25,14 +31,29 @@ export default function Home() {
     });
   };
 
+  useEffect(() => {
+    if (isStarted) {
+      setInterval(() => {
+        if (timer > 0) setTimer(timer - 1);
+      }, 1000);
+    }
+  }, [timer]);
+
   return (
     <div>
       <h1>Typing Game</h1>
+      <p>{timer}</p>
       <p>{renderText()}</p>
       <input
         type="text"
         value={userInput}
-        onChange={handleInputChange}
+        onChange={(e) => {
+          handleInputChange(e);
+          if (userInput.length === 0) {
+            setIsStarted(true);
+            setTimer(timer - 1);
+          }
+        }}
         placeholder="Write here..."
         style={{ width: "100%", padding: "10px", fontSize: "16px" }}
       />
